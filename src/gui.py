a1734d1cfe26b24803db97e19eb5d7a53e3f7728
luckyhut <ed7johnson@gmail.com>
2025-03-22 16:41:05 -0600

    Moved dice code to another file; gui uses it now

--- src/gui.py
+++ src/gui.py
@@ -1,10 +1,17 @@
 from tkinter import *
 from tkinter import ttk
+from dice import roll_dice
 
 def start_window():
+    # Get a d6 and a d20 roll
+    d6 = roll_dice(6)
+    d20 = roll_dice(20)
+    output = f"Hello World \nd6 roll: {d6} \nd20 roll: {d20}"
+
+    # Tkinter setup
     root = Tk()
     frm = ttk.Frame(root, padding=10)
     frm.grid()
-    ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+    ttk.Label(frm, text=output).grid(column=0, row=0)
     ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
     root.mainloop()
--- src/main.py
+++ src/main.py
@@ -1,17 +1,7 @@
-import random as rand
 from gui import *
 
 def main():
-    results = []
-    rand.seed(0)
-    results.append(roll_dice(6))
-    results.append(roll_dice(10))
-    results.append(roll_dice(12))
-    results.append(roll_dice(20))
-    print(results)
-    start_window()
 
-def roll_dice(faces: int):
-    return rand.randint(1, faces)
+    start_window()
 
 main()
